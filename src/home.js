76602771a35dc287f17ef2593068aad69cfaea51
andkymon <100330163+andkymon@users.noreply.github.com>
2024-12-13 14:56:04 +0800

    Reorganize home.js/Event listeners for index.js
    home.js:
    - Added comments on home.js for readability
    - Reorganized code so it starts from the deepest nested element
    index,js:
    - imported loadMenu to loadMenu page
    - invoke loadHome on document load
    - add clearPage() function for clearing the page before appending
    contents of the selected page
    - add event listeners for home and menu nav buttons

--- src/home.js
+++ src/home.js
@@ -1,23 +1,27 @@
 function loadHome() {
-    const content = document.querySelector("#content");
     const home = document.createElement("div");
     const headline = document.createElement("h1");
-    const attributionDiv = document.createElement("span");
+    headline.textContent = "Flavors That Evolve Without End.";
+    home.append(headline);
+    
+    //Attribution 
+    const attributionDiv = document.createElement("div");
     const attributionSpan = document.createElement("span");
     const author = document.createElement("a");
     const site = document.createElement("a");
-
-    home.setAttribute("id", "home");
-    headline.textContent = "Flavors That Evolve Without End.";
-
     author.setAttribute("href", "https://unsplash.com/@ahtziri");
     author.textContent = "Ahtziri Lagarde";
     site.setAttribute("href", "https://unsplash.com");
     site.textContent = "Unsplash";
     attributionSpan.append("Photo by ", author, " on ", site);
     attributionDiv.append(attributionSpan);
+    attributionDiv.setAttribute("id", "attribution");
+    home.append(attributionDiv);
     
-    home.append(headline, attributionDiv);
+    //Append #home div to #content div
+    const content = document.querySelector("#content");
+    home.classList.add("page");
+    home.setAttribute("id", "home");
     content.append(home);
 };
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
 import css from "./style.css"
 import loadHome from "./home.js"
-loadHome();
+import loadMenu from "./menu.js"
+
+//Event listener for document load
+document.addEventListener("DOMContentLoaded", function() {
+    loadHome();
+});
+
+//Event listeners for nav buttons
+const content = document.querySelector("#content");
+const buttons = document.querySelectorAll("button");
+
+function clearPage() {
+    const page = document.querySelector(".page");
+    content.removeChild(page);
+}
+
+buttons[0].addEventListener("click", () => {
+    clearPage();
+    loadHome();
+});
+buttons[1].addEventListener("click", () => {
+    clearPage();
+    loadMenu();
+});
+
 
